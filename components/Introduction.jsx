02ab98648b4caa758e7091387509fc8f94f728f2
dpgaire <gairhedurga13@gmail.com>
2024-01-25 14:45:32 +0545

    intro-dynamic-data

--- components/Introduction.jsx
+++ components/Introduction.jsx
@@ -1,21 +1,32 @@
 // components/Introduction.js
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 
 const Introduction = () => {
+
+  const [profile, setProfile] = useState({});
+
+  useEffect(() => {
+    fetch('/api/intoduction') 
+      .then((response) => response.json())
+      .then((data) => setProfile(data.myInfo))
+      .catch((error) => console.error('Error fetching projects:', error));
+  }, []);
+
   return (
     <section id="about" className="h-screen flex items-center justify-center bg-cover bg-center relative" style={{ backgroundImage: "url('/images/background.png')" }}>
       <div className="text-center text-white z-10">
         {/* Cube-like Structure (Profile Picture) */}
         <div className="w-32 h-32 bg-indigo-500 rounded-full mx-auto mb-4">
-        <img src="/images/profile-pic.png" alt="Profile" className="w-full h-full rounded-full mx-auto mb-4" />
+        <img src={'/images/profile-pic.png'} alt="Profile" className="w-full h-full rounded-full mx-auto mb-4" />
         </div>
         {/* Title and Subtitle */}
-        <h1 className="text-4xl font-bold mb-2">Hi, I'm Durga Gairhe.</h1>
-        <h2 className="text-xl mb-4">Software Engineer</h2>
+        <h1 className="text-4xl font-bold mb-2">{profile.title}</h1>
+        <h2 className="text-xl mb-4">{profile.deginations}</h2>
 
         {/* Description */}
-        <p className="text-lg mb-8 max-w-md mx-auto">High level experience in web design and development knowledge with producing quality work.</p>
+        <p className="text-lg mb-8 max-w-md mx-auto">{profile.description}</p>
 
         {/* Call-to-action button */}
         <Link  href="#projects" className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-700 transition duration-300">View Projects</Link>
---
+++ pages/api/intoduction.js
@@ -0,0 +1,16 @@
+
+const myInfo = {
+    title: "Hi, I'm Durga Gairhe.",
+    degination: "Software Engineer",
+    description: "High level experience in web design and development knowledge with producing quality work.",
+    profile_image:"/images/profile-pic.png"
+}
+
+  export default (req, res) => {
+    if (req.method === 'GET') {
+      res.status(200).json({ myInfo });
+    } else {
+      res.status(405).json({ error: 'Method Not Allowed' });
+    }
+  };
+  
