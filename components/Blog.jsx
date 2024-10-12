c43741b2e162dc393c2303405ec94a8cecbc1e7b
dpgaire <gairhedurga13@gmail.com>
2024-10-12 19:16:37 +0545

    update blog section

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -4,6 +4,7 @@ import { HEADER_CONTENT } from "@/data";
 import SocialLinks from "./SocialLinks";
 import { SubmitButton } from "./ui";
 import { BlogCard } from "./ui/cards";
+import BlogLists from "./skeletons/BlogLists";
 
 const { content, email, footerDescription } = HEADER_CONTENT;
 
@@ -22,6 +23,9 @@ const Blog = ({ blogs }) => {
         </div>
       </PageHeader>
       <div className="grid grid-cols-1 mt-4 sm:grid-cols-2 lg:grid-cols-3 gap-8 md:p-0 p-2">
+        {/* {Array.from({ length: 6 }).map((_, index) => (
+          <BlogLists key={index} />
+        ))} */}
         {blogs.map((blog) => (
           <BlogCard
             key={blog.id}
